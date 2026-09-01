433a94c3970fbdebba49006cd5ee8c602047e96f
DevCorbetto-tvp <gabriel.corbetto@vnstudios.com>
2026-09-01 17:38:27 -0500

    w

--- assets/g4u-scroll-carousel.js
+++ assets/g4u-scroll-carousel.js
@@ -4,8 +4,25 @@ if (!customElements.get('g4u-scroll-carousel')) {
     class G4uScrollCarousel extends HTMLElement {
       connectedCallback() {
         this.track = this.querySelector('[data-g4u-scroll-track]');
-        this.querySelector('[data-g4u-scroll-prev]')?.addEventListener('click', () => this.move(-1));
-        this.querySelector('[data-g4u-scroll-next]')?.addEventListener('click', () => this.move(1));
+        this.previousButton = this.querySelector('[data-g4u-scroll-prev]');
+        this.nextButton = this.querySelector('[data-g4u-scroll-next]');
+        this.previousButton?.addEventListener('click', () => this.move(-1));
+        this.nextButton?.addEventListener('click', () => this.move(1));
+        this.track?.addEventListener('scroll', () => this.updateControls(), { passive: true });
+        this.resizeObserver = new ResizeObserver(() => this.updateControls());
+        if (this.track) this.resizeObserver.observe(this.track);
+        this.updateControls();
+      }
+
+      disconnectedCallback() { this.resizeObserver?.disconnect(); }
+
+      updateControls() {
+        if (!this.track) return;
+        const maximumScroll = Math.max(0, this.track.scrollWidth - this.track.clientWidth);
+        const isStatic = maximumScroll < 2;
+        this.toggleAttribute('data-g4u-static', isStatic);
+        if (this.previousButton) this.previousButton.disabled = isStatic || this.track.scrollLeft <= 1;
+        if (this.nextButton) this.nextButton.disabled = isStatic || this.track.scrollLeft >= maximumScroll - 1;
       }
 
       move(direction) {
